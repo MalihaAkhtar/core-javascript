b0e8c9f87f167690ce436e4026084c85f9dd562f
Maliha Akhtar <145904037+MalihaAkhtar@users.noreply.github.com>
2024-08-28 20:49:40 +0500

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,7 @@
+var fullName ="Maliha Akhtar";
+//console.log(fullName);
+let department = "computer science" ;
+//console.log(department);
+const studentId = 14;
+//console.log(studentId);
+console.table([fullName,department,studentId])
