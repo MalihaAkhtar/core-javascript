0a0954cfe27b93f90a2a086ae9c3d2ac978f977b
Maliha Akhtar <145904037+MalihaAkhtar@users.noreply.github.com>
2024-08-31 10:56:24 +0500

    Add files via upload

---
+++ comparison.js
@@ -0,0 +1,21 @@
+//***************************comparison **************************************
+let a=2;
+let b= 3;
+console.log(a>b);
+console.log(a<b);
+console.log(a==b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a!==b);
+//****************************comparison with null*****************************
+console.log(null==0);
+console.log(null>0);
+console.log(null>=0);
+//**************************comparison with undefined*************************** 
+console.log(undefined>0);
+console.log(undefined>=0);
+//************************comparison with string********************************* 
+console.log("z">"A");
+console.log('2'>'A');
+console.log('a'>'A');
+console.log('Glow'>'Glee');
